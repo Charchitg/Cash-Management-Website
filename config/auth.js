3cdfd915d9ea3cf9bdc2fb3d4a79b80e6bbfa661
Charchitg <Charchitg07@gmail.com>
2021-06-08 16:48:54 +0530

    final commit

--- config/auth.js
+++ config/auth.js
@@ -4,14 +4,14 @@ module.exports = {
         if(req.isAuthenticated()){
             return next();
         }
-        console.log('User Login Required');
+        req.flash('error_msg' , 'User Login Required');
         res.redirect('/user/login');
     } , 
     NotAuthenticated : function(req,res,next){
         if(!req.isAuthenticated()){
             return next();
         }
-        console.log('User Already logged in');
+        req.flash('error_msg','User Already logged in');
         res.redirect('/user/home');
     }
 }    
